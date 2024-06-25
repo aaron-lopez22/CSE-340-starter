35bb15a18cb3ddeeff2e25e0ccbcae5120eef479
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2024-06-24 21:16:37 -0600

    comment

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -21,6 +21,7 @@ UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
 
+--update to query 4 and 6
 
 UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')

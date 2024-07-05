c5d653a6b64192ec0c1cc6a0885616864212d2c4
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2024-07-05 17:52:07 -0600

    fixingtheimages

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require('./utilities/');
 
 app.use(express.static('public'))
 app.use(expressLayouts)
 app.use(static)
+
 
 
 /* ***********************
@@ -24,11 +26,30 @@ app.use(static)
 //index route
 app.set("view engine", "ejs")
 app.set("layout", "./layouts/layout") // not at views root
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
 app.use("/inv", inventoryRoute)
+
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 
 
